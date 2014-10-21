0cca7450c6cb3f4092c8047dc67ab6107cdc7878
Brian Lau <brian.lau@upmc.fr>
2014-10-21 20:05:55 +0200

    BF: sync working again

--- @SampledProcess/sync.m
+++ @SampledProcess/sync.m
@@ -1,17 +1,20 @@
-%      function self = sync(self,event,varargin)
 % should allow resampling
-function [values,times] = sync(self,event,varargin)
+function s = sync(self,event,varargin)
 p = inputParser;
 p.KeepUnmatched= false;
 p.FunctionName = 'SampledProcess sync';
 p.addRequired('event',@(x) isnumeric(x));
-p.addParamValue('window',[]);
+p.addOptional('window',[],@(x) isnumeric(x) && (size(x,1)==1) && (size(x,2)==2)); 
 %p.addParamValue('resample',[]);
 p.parse(event,varargin{:});
+
+assert(numel(event)==numel(self),'SampledProcess:sync:InputValue',...
+   'numel(event) should match numel(SampledProcess)');
 
 self.setInclusiveWindow;
 
 if isempty(p.Results.window)
+   % find window that includes all data
    temp = vertcat(self.window);
    temp = bsxfun(@minus,temp,event(:));
    window = [min(temp(:,1)) max(temp(:,2))];
@@ -21,23 +24,19 @@ else
 end
 
 nObj = numel(self);
-if size(window,1) == 1
-   window = repmat(window,nObj,1);
-   window = bsxfun(@plus,window,event(:));
-   %window = mat2cell(window,ones(nObj,1),2);
-   
-   self.setWindow(window);
-   self.setOffset(-event);
-   
-   fixedWindow = true;
-else
-   error('not done')
-end
+window = repmat(window,nObj,1);
+window = bsxfun(@plus,window,event(:));
+window = mat2cell(window,ones(nObj,1),2);
+
+self.setWindow(window);
+self.setOffset(-event);
+
+fixedWindow = true; % THIS MAY NEVER BE FALSE?
 
 if nargout
    [times,values] = arrayfun(@(x) deal(x.times{1},x.values{1}),self,'uni',false);
    if fixedWindow
-      times = times{1};
-      values = cell2mat(values);
+      s.times = times{1};
+      s.values = cell2mat(values);
    end
 end
