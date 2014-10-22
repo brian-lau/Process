1c25a3ff81059a74967c751868d716edfc33fa93
Brian Lau <brian.lau@upmc.fr>
2014-10-22 15:11:01 +0200

    BF: checks labels for each process

--- @SampledProcess/extract.m
+++ @SampledProcess/extract.m
@@ -1,10 +1,13 @@
-% values,times output? same order for Sampled/Point?
+% Extract times & values
 function [s,labels] = extract(self,reqLabels)
 
 s(numel(self),1) = struct('times',[],'values',[]);
 labels = cell(numel(self),1);
 for i = 1:numel(self)
-   [labels{i,1},~,ind] = intersect(reqLabels,self.labels,'stable');
+   if nargin < 2
+      reqLabels = self(i).labels;
+   end
+   [labels{i,1},~,ind] = intersect(reqLabels,self(i).labels,'stable');
    
    if any(ind)
       if size(self(i).window,1) == 1
