36c65733d04190387c9950aab5ddf6961d4f7bb1
Brian Lau <brian.lau@upmc.fr>
2014-10-22 15:31:22 +0200

    ENH: resample works with object arrays

--- @SampledProcess/resample.m
+++ @SampledProcess/resample.m
@@ -1,25 +1,27 @@
-%http://www.mathworks.fr/fr/help/matlab/ref/timeseries.synchronize.html?refresh=true
-%http://www.mathworks.com/matlabcentral/fileexchange/12268-sinc-resample
-function self = resample(self,newFs)
-% use lcm?
-% http://www.mathworks.com/matlabcentral/fileexchange/45329-sample-rate-conversion/content/SRC/srconv.m
-
 % resample data in window
-if self.Fs == newFs
-   return;
-end
-[p,q] = rat(newFs/self.Fs);
-%values = cellfun(@(x) resample(x,p,q),self.values,'uni',0);
-[values{1},b] = resample(self.values{1},p,q);
-nWindow = size(self.window,1);
-if nWindow > 1
-   values(2:nWindow,1) = cellfun(@(x) resample(x,p,q,b),...
-      self.values(2:nWindow,1),'uni',0);
-end
+function self = resample(self,newFs)
 
-times = cellfun(@(x,y) self.tvec(x(1),1/newFs,size(y,1)),...
-   self.times,values,'uni',0);
-
-self.times = times;
-self.values = values;
-self.Fs = newFs;
+for i = 1:numel(self)
+   if self(i).Fs == newFs
+      continue;
+   end
+   
+   % use lcm?
+   % http://www.mathworks.com/matlabcentral/fileexchange/45329-sample-rate-conversion/content/SRC/srconv.m
+   [p,q] = rat(newFs/self(i).Fs);
+   
+   % Resample first window and cache the filter
+   [values{1},b] = resample(self(i).values{1},p,q);
+   nWindow = size(self(i).window,1);
+   if nWindow > 1
+      values(2:nWindow,1) = cellfun(@(x) resample(x,p,q,b),...
+         self(i).values(2:nWindow,1),'uni',0);
+   end
+   
+   times = cellfun(@(x,y) self(i).tvec(x(1),1/newFs,size(y,1)),...
+      self(i).times,values,'uni',0);
+   
+   self(i).times = times;
+   self(i).values = values;
+   self(i).Fs = newFs;
+end
